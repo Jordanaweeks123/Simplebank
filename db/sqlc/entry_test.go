51d3e1e7309ec4b7b0863a8192756a73661120c6
Jordan Weeks <jordanweeks123.jw@gmail.com>
2023-10-29 23:57:38 -0500

    finished create and get entry unit tests

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -3,13 +3,13 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/techschool/simplebank/util"
 )
 
-// Test CreateEntry
-func TestCreateEntry(t *testing.T) {
+func createRandomEntry(t *testing.T) Entry {
 	account := createRandomAccount(t)
 
 	args := CreateEntryParams{
@@ -24,6 +24,27 @@ func TestCreateEntry(t *testing.T) {
 
 	require.Equal(t, entry.AccountID, account.ID)
 	require.Equal(t, entry.Amount, args.Amount)
+
+	return entry
+}
+
+// Test CreateEntry
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+	require.Equal(t, entry1.ID, entry2.ID)
 }
 
 // Test DeleteEntry
